8697a0a345d8b2d0cc982961f260dfcf59f30e8d
Daoyi <daoyi.roodt@gmail.com>
2023-10-17 17:41:29 +0200

    Adds some grammar checks and fixes bugs to gendered words replacement

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import {redact} from './src/redact';
 
-let cv:string = "Alice won the best actress award in 2013. She was the first person on the moon.";
+let cv:string = "Alice won the best actress award in 2013. An actress's greatest dream. She was a baroness. Mother. Queen. A girl in a big world. A waitress working nine till six. She was the first woman on the moon.";
 let name:string = "Alice";
 
-console.log(cv);
-console.log(redact(cv, name));
+let new_cv:string = redact(cv, name);
+
+console.log("Original: " + cv);
+console.log("Redacted: " + new_cv);
--- out/src/redact.js
+++ out/src/redact.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redact = void 0;
 var gender_1 = require("./gender/gender");
+var gender_2 = require("./gender/gender");
 function redact(cv, name) {
     var words = cv.split(" ");
     words.forEach(function (value, index) {
-        words[index] = (0, gender_1.replace_gender)(value);
+        words[index] = (0, gender_1.replaceGender)(value);
+        if (index > 0 && words[index - 1].toLocaleLowerCase() == "an" && words[index].charAt(0).match(/[aeiou]/) == null) {
+            words[index - 1] == (0, gender_2.capitalizeFirstLetter)(words[index - 1]) ?
+                words[index - 1] = "A" : words[index - 1] = "a";
+        }
     });
     return (words.join(" "));
 }
