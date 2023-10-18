0b7763c6cc67368bccf5a5667f8be3924744a912
Michelle <m.stegeman@me.com>
2023-10-18 18:25:45 +0200

    updated phone# regex

--- redact_src/contacts.ts
+++ redact_src/contacts.ts
@@ -1,14 +1,14 @@
 export function redactContacts(cv:string)
 {
-	const linkedin = /https:\/\/(www\.)?linkedin\.com\S*/gi 
-	const twitter = /https:\/\/(www\.)?twitter\.com\S*/gi 
-	const facebook = /https:\/\/(www\.)?facebook\.com\S*/gi 
-	const instagram = /https:\/\/(www\.)?instagram\.com\S*/gi 
-	const github = /https:\/\/(www\.)?github\.com\S*/gi 
+	const linkedin = /https:\/\/(www\.)?linkedin\.com\S*/gi
+	const twitter = /https:\/\/(www\.)?twitter\.com\S*/gi
+	const facebook = /https:\/\/(www\.)?facebook\.com\S*/gi
+	const instagram = /https:\/\/(www\.)?instagram\.com\S*/gi
+	const github = /https:\/\/(www\.)?github\.com\S*/gi
 	// const phoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-	const phoneNumberRegex = /\+\w+\s[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,4}/g;
+	const phoneNumberRegex = /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,5}/g;
 	// const email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b/gi;
-	
+
 	// Replace matched patterns with asterisks
 	cv = cv.replace(linkedin, '********');
 	cv = cv.replace(twitter, '********');
